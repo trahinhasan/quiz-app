4251b9f3cb753d8b1325c5c4f53075d015828054
rahin505 <trahinhasan@gmail.com>
2022-08-04 16:22:49 +0200

    first attempt

---
+++ script.js
@@ -0,0 +1,40 @@
+const quizData = [
+    {
+        question: 'How old is Florin?',
+        a: '10',
+        b: '17',
+        c: '26',
+        d: '22',
+        correct: 'c'
+    }, {
+        question: 'What is the most used programming language in 2019?',
+        a: 'Javascript',
+        b: 'Python',
+        c: 'Java',
+        d: 'C++',
+        correct: 'd'
+    }, {
+        question: 'Who is the president of US?',
+        a: 'Florian Pop',
+        b: 'Joe Biden',
+        c: 'Donald Trump',
+        d: 'Mithai Andrei',
+        correct: 'b'
+    }, {
+        question: 'What is HTML stands for?',
+        a: 'Hypertext Markup Language',
+        b: 'Cascading Style Sheet',
+        c: 'Jason Object Notation',
+        d: 'Halicopter Terminal Motor Lambo',
+        correct: 'a'
+    }, {
+        question: 'What year was Javascript Launched?',
+        a: '1996',
+        b: '1995',
+        c: '1994',
+        d: 'none of above',
+        correct: 'd'
+    }
+
+
+]
